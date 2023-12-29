c7978d59a057fbd0ef61d8ca93be95ff487a0a9e
Sebastiao Rogerio <sebast.rogers@gmail.com>
2023-12-29 18:23:18 -0300

    exemplos basicos

---
+++ type-string.js
@@ -0,0 +1,27 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+//template string OU template literal
+
+
+
+//
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+//
+const senha2 = "minhaSenha123"
+console.log(senha.length) // 13 caracteres
