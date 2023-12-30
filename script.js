5b173709c796b8a5127c2c82dd2bda1067a9a4fa
Sebastiao Rogerio <sebast.rogers@gmail.com>
2023-12-30 18:27:38 -0300

    codigos var e conversoes

---
+++ script.js
@@ -0,0 +1,45 @@
+//var
+
+// var altura = 5;
+// var comprimento = 7;
+
+// area = altura * comprimento
+// console.log(area)
+
+// // é possível usar "var" ela antes de declarar
+// var area;
+
+/////////////////////////////
+
+//let
+// let altura = 5;
+// let comprimento = 7;
+// let forma = 'retângulo';
+// let area;
+
+// if(forma === 'retângulo') {
+//     area = altura * comprimento;
+// } else {
+//     area = (altura * comprimento) / 2;
+// }
+
+
+// console.log(area)
+
+//let area;
+
+/////////////////////////////
+
+const forma = 'triângulo';
+const altura = 5;
+const comprimento = 7;
+//const area = altura * comprimento;
+let area;
+
+if (forma === 'quadrado') {
+    area = altura * comprimento
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area);
