64b99919e90a69cbc8f73ef5247d7eaad6b047ea
Sebastiao Rogerio <sebast.rogers@gmail.com>
2023-12-30 20:00:49 -0300

    novos exemplos

---
+++ erros.js
@@ -0,0 +1,40 @@
+const numero = 0;
+// const minhaVar = "oi";
+console.log(minhVar);
+
+///////////////
+// Endereço/do/arquivo/erros.js]:1
+
+// const numero;
+
+// SyntaxError: missing initializer in const declaration
+
+// [Mensagem omitida]
+
+////////////////
+// Endereço/do/arquivo/erros.js]:2
+
+// console.log(numero
+
+// SyntaxError: missing ) after argument list
+
+// [Mensagem omitida]
+
+////////////////
+// Endereço/do/arquivo/erros.js]:2
+
+// console.log(minhaVar);
+
+// ReferenceError: minhaVar is not defined
+
+// [Mensagem omitida]
+
+
+/////////////////
+// Endereço/do/arquivo/erros.js]:1
+
+// const numero;
+
+// SyntaxError: Missing initializer in const declaration
+
+// at Object < anonymous > (endereço/do/arquivo/erros.js:3:13)
